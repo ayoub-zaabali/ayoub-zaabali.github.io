431b25979e0239e4b46a84d470bd4cda0734f18f
Firat Edis <Firat.edis@sidenodes.com>
2023-09-21 15:51:44 +0000

    fixe contact form

--- js/post.js
+++ js/post.js
@@ -1,39 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.forms["contact-form"].addEventListener("submit", postData);
+document.addEventListener("DOMContentLoaded", function () {
+  document.forms["contact-form"].addEventListener("submit", postData);
 });
 
 function postData(formsubmission) {
-    formsubmission.preventDefault();
+  formsubmission.preventDefault();
 
-    var name = encodeURIComponent(document.getElementById("name").value);
-    var email = encodeURIComponent(document.querySelector("input[type='email']").value);
-    var message = encodeURIComponent(document.getElementById("message").value);
+  var name = encodeURIComponent(document.getElementById("name").value);
+  var email = encodeURIComponent(
+    document.querySelector("input[type='email']").value
+  );
+  var message = encodeURIComponent(document.getElementById("message").value);
 
-    // Checks if fields are filled-in or not, returns response "<p>Please enter your details.</p>" if not.
-    if (name == "" || email == "" || message == "") {
-        document.getElementById("response").innerHTML = "<p>Please enter your details.</p>";
-        return;
+  // Checks if fields are filled-in or not, returns response "<p>Please enter your details.</p>" if not.
+  if (name == "" || email == "" || message == "") {
+    document.getElementById("response").innerHTML =
+      "<p>Please enter your details.</p>";
+    return;
+  }
+
+  // Parameters to send to PHP script. The bits in the "quotes" are the POST indexes to be sent to the PHP script.
+  var params = "name=" + name + "&email=" + email + "&message=" + message;
+
+  var http = new XMLHttpRequest();
+  http.open("POST", "https://sendmail-production.up.railway.app/", true);
+
+  // Set headers
+  http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  http.onreadystatechange = function () {
+    if (http.readyState == 4 && http.status == 200) {
+      if (http.responseText) {
+        document.getElementById("response").innerHTML =
+          "<label class='ok'>Message Sent<i class='fa-solid fa-circle-check'></i></label>";
+      } else {
+        document.getElementById("response").innerHTML =
+          "<label class='error' >An unknown error occurred while sending the email message<i class='fa-solid fa-circle-exclamation'></i></label>";
+      }
     }
-
-    // Parameters to send to PHP script. The bits in the "quotes" are the POST indexes to be sent to the PHP script.
-    var params = "name=" + name + "&email=" + email + "&message=" + message;
-
-    var http = new XMLHttpRequest();
-    http.open("POST", "https://eemailee.herokuapp.com/", true);
-
-    // Set headers
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    http.onreadystatechange = function() {
-        if (http.readyState == 4 && http.status == 200) {
-            if (http.responseText) {
-                document.getElementById("response").innerHTML = "<label class='ok'>Message Sent<i class='fa-solid fa-circle-check'></i></label>";
-            } else {
-                document.getElementById("response").innerHTML = "<label class='error' >An unknown error occurred while sending the email message<i class='fa-solid fa-circle-exclamation'></i></label>";
-            }
-        }
-    }
-    http.send(params);
-    document.getElementById("response").innerHTML = "<img src='images/Rolling.svg'>";
-
+  };
+  http.send(params);
+  document.getElementById("response").innerHTML =
+    "<img src='images/Rolling.svg'>";
 }
